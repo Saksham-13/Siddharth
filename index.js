3dc5188bec0abaaf34d6787cfcb300ab3f28742f
Saksham-13 <sakshamalok13@gmail.com>
2022-09-11 14:24:35 +0530

    added sme shit

--- index.js
+++ index.js
@@ -2,12 +2,16 @@
   "use strict";
 
   $(".card").tilt({
+
     maxTilt: 25,
-    perspective: 1400,
+    perspective: 1300,
+    
     easing: "cubic-bezier(.03,.98,.52,.99)",
     speed: 1200,
     glare: true,
     maxGlare: 0.5,
-    scale: 1.1
+    scale: 1.1,
+    
+
   });
 })(jQuery);
